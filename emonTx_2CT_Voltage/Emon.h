88b08dee349b0957fd7a79f47fddd7c7d1e65767
Glyn Hudson <glyn.hudson@gmail.com>
2011-12-28 23:39:00 +0000

    added 2CT voltage example

---
+++ emonTx_2CT_Voltage/Emon.h
@@ -0,0 +1,59 @@
+/*
+  Emon.h - Library for openenergymonitor
+  Created by Trystan Lea, April 27 2010
+  GNU GPL
+*/
+
+#ifndef Emon_h
+#define Emon_h
+
+#include "WProgram.h"
+
+class EnergyMonitor
+{
+  public:
+
+    void setPins(int _inPinV,int _inPinI);
+    void calibration(double _VCAL, double _ICAL, double _PHASECAL);
+    void calc(int wavelengths, int timeout, int SUPPLYVOLTAGE);
+
+    //Useful value variables
+    double realPower,
+       apparentPower,
+       powerFactor,
+       Vrms,
+       Irms;
+
+  private:
+
+    //Set Voltage and current input pins
+    int inPinV;
+    int inPinI;
+    //Calibration coeficients
+    //These need to be set in order to obtain accurate results
+    double VCAL;
+    double ICAL;
+    double PHASECAL;
+
+    //--------------------------------------------------------------------------------------
+    // Variable declaration for emon_calc procedure
+    //--------------------------------------------------------------------------------------
+	int lastSampleV,sampleV;   //sample_ holds the raw analog read value, lastSample_ holds the last sample
+	int lastSampleI,sampleI;                      
+
+	double lastFilteredV,filteredV;                   //Filtered_ is the raw analog value minus the DC offset
+	double lastFilteredI, filteredI;                  
+
+	double phaseShiftedV;                             //Holds the calibrated phase shifted voltage.
+
+	double sqV,sumV,sqI,sumI,instP,sumP;              //sq = squared, sum = Sum, inst = instantaneous
+
+	int startV;                                       //Instantaneous voltage at start of sample window.
+
+	boolean lastVCross, checkVCross;                  //Used to measure number of times threshold is crossed.
+	int crossCount;                                   // ''
+
+
+};
+
+#endif
