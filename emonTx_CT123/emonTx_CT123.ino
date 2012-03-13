b95c500ec5cb0d26ce0b89241c4f686bc222e829
Trystan <trystan@trystan-laptop.(none)>
2012-03-13 17:26:16 +0000

    Minor changes

--- emonTx_CT123/emonTx_CT123.ino
+++ emonTx_CT123/emonTx_CT123.ino
@@ -5,7 +5,9 @@
 // Builds upon JeeLabs RF12 library and Arduino
 //
 // When doing a long term instal its worth deleting
-// the Serial.begin and print statements 
+// the Serial.begin and print statements
+
+#define freq RF12_433MHZ     //Frequency of RF12B module can be RF12_433MHZ, RF12_868MHZ or RF12_915MHZ. You should use the one matching the module you have.
 
 #define UNO                                                             // Uncomment this line if your not using
 #include <avr/wdt.h>                                                    // the UNO bootloader 
@@ -15,10 +17,15 @@ ISR(WDT_vect) { Sleepy::watchdogEvent(); }
 
 #include "EmonLib.h"
 EnergyMonitor emon1;                                                    // Create  instances for each CT channel
-EnergyMonitor emon2;
-EnergyMonitor emon3;
+// EnergyMonitor emon2;                                                 // Uncomment for 2 CT's
+// EnergyMonitor emon3;                                                 // Uncomment for 3 CT's
 
-typedef struct { int power1, power2, power3, battery; } PayloadTX;      // Create emontx data structure
+typedef struct {                                                        // Create emontx data structure
+  int power1;
+  // int power2;                                                        // Uncomment for 2 CT's
+  // int power3;                                                        // Uncomment for 3 CT's
+  int battery;
+} PayloadTX;
 PayloadTX emontx;                                                       // neat way of packaging data for RF comms
 
 # define LEDpin 9
@@ -29,10 +36,10 @@ void setup()
   Serial.println("emonTX CT123 example");
              
   emon1.currentTX(1, 115.6);                                            // Setup emonTX CT channel (channel, calibration)
-  emon2.currentTX(2, 115.6);
-  emon3.currentTX(3, 115.6);
+  // emon2.currentTX(2, 115.6);                                         // Uncomment for 2 CT's
+  // emon3.currentTX(3, 115.6);                                         // Uncomment for 3 CT's
   
-  rf12_initialize(10, RF12_433MHZ, 210);                                // initialize RF
+  rf12_initialize(10, freq, 210);                                // initialize RF
   rf12_sleep(RF12_SLEEP);
 
   pinMode(LEDpin, OUTPUT);                                              // Setup indicator LED
@@ -46,15 +53,15 @@ void setup()
 void loop() 
 { 
   emontx.power1 = emon1.calcIrms(1480) * 240.0;                         // Calculate CT 1 power
-  emontx.power2 = emon2.calcIrms(1480) * 240.0;
-  emontx.power3 = emon3.calcIrms(1480) * 240.0;
+  // emontx.power2 = emon2.calcIrms(1480) * 240.0;                      // Uncomment for 2 CT's
+  // emontx.power3 = emon3.calcIrms(1480) * 240.0;                      // Uncomment for 3 CT's
+  
   emontx.battery = emon1.readVcc();
   
   Serial.print(emontx.power1);                                          // Output to serial
-  Serial.print(" ");
-  Serial.print(emontx.power2);
-  Serial.print(" ");
-  Serial.println(emontx.power3);
+  // Serial.print(" "); Serial.print(emontx.power2);                    // Uncomment to print 2nd CT
+  // Serial.print(" "); Serial.print(emontx.power3);                    // Uncomment to print 3rd CT
+  Serial.println();
  
   send_rf_data();                                                       // *SEND RF DATA* - see emontx_lib
   emontx_sleep(10);                                                     // sleep or delay in seconds - see emontx_lib
