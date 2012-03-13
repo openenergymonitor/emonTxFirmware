d82ffbb26f3efc16656d7e3e19f5883b69eb1d49
Ian Chilton <ian@ichilton.co.uk>
2012-03-13 21:33:28 +0000

    Make CT2 and CT3 configurable

--- emonTx_CT123/emonTx_CT123.ino
+++ emonTx_CT123/emonTx_CT123.ino
@@ -7,9 +7,15 @@
 // When doing a long term instal its worth deleting
 // the Serial.begin and print statements
 
+
+// #define CT2								// Uncomment for CT2
+// #define CT3								// Uncomment for CT3
+
 #define freq RF12_433MHZ                                                //Frequency of RF12B module can be RF12_433MHZ, RF12_868MHZ or RF12_915MHZ. You should use the one matching the module you have.
 #define nodeID 10                                                       //emonTx node ID
 #define networkGroup 210                                                //emonTx RFM12B wireless network group - needs to be same as emonBase and emonGLCD
+
+
 
 #define UNO                                                             // Uncomment this line if your not using the UNO bootloader (i.e using Duemilanove) - All Atmega's shipped from OpenEnergyMonitor come witj Arduino Uno bootloader
 #include <avr/wdt.h>                                                    // 
@@ -19,13 +25,19 @@ ISR(WDT_vect) { Sleepy::watchdogEvent(); }                              //Attach
 
 #include "EmonLib.h"
 EnergyMonitor emon1;                                                    // Create  instances for each CT channel
-// EnergyMonitor emon2;                                                 // Uncomment for 2 CT's
-// EnergyMonitor emon3;                                                 // Uncomment for 3 CT's
+
+#ifdef CT2
+  EnergyMonitor emon2;
+#endif
+
+#ifdef CT3
+  EnergyMonitor emon3;
+#endif
 
 typedef struct {                                                        // Create emontx data structure
   int power1;
-  // int power2;                                                        // Uncomment for 2 CT's
-  // int power3;                                                        // Uncomment for 3 CT's
+  int power2;
+  int power3;
   int battery;
 } PayloadTX;
 PayloadTX emontx;                                                       // neat way of packaging data for RF comms
@@ -38,8 +50,14 @@ void setup()
   Serial.println("emonTX CT123 example"); Serial.println("OpenEnergyMonitor.org");
              
   emon1.currentTX(1, 115.6);                                            // Setup emonTX CT channel (channel, calibration)
-  // emon2.currentTX(2, 115.6);                                         // Uncomment for 2 CT's
-  // emon3.currentTX(3, 115.6);                                         // Uncomment for 3 CT's
+
+  #ifdef CT2
+    emon2.currentTX(2, 115.6);
+  #endif
+
+  #ifdef CT3
+    emon3.currentTX(3, 115.6);
+  #endif
   
   rf12_initialize(nodeID, freq, networkGroup);                          // initialize RFM12B
   rf12_sleep(RF12_SLEEP);
@@ -55,14 +73,27 @@ void setup()
 void loop() 
 { 
   emontx.power1 = emon1.calcIrms(1480) * 240.0;                         // Calculate CT 1 power
-  // emontx.power2 = emon2.calcIrms(1480) * 240.0;                      // Uncomment for 2 CT's
-  // emontx.power3 = emon3.calcIrms(1480) * 240.0;                      // Uncomment for 3 CT's
+
+  #ifdef CT2
+    emontx.power2 = emon2.calcIrms(1480) * 240.0;
+  #endif
+
+  #ifdef CT3
+    emontx.power3 = emon3.calcIrms(1480) * 240.0;
+  #endif
   
   emontx.battery = emon1.readVcc();
   
   Serial.print(emontx.power1);                                          // Output to serial
-  // Serial.print(" "); Serial.print(emontx.power2);                    // Uncomment to print 2nd CT
-  // Serial.print(" "); Serial.print(emontx.power3);                    // Uncomment to print 3rd CT
+
+  #ifdef CT2
+    Serial.print(" "); Serial.print(emontx.power2);
+  #endif
+
+  #ifdef CT3
+    Serial.print(" "); Serial.print(emontx.power3);
+  #endif
+
   Serial.println();
  
   send_rf_data();                                                       // *SEND RF DATA* - see emontx_lib
