cd2e1100457a1a29f217e65b0fa70aaec3593e43
glynhudson <glyn@glyn-ubuntu13.04>
2013-05-13 15:09:57 +0100

    fix changes

--- emonTx_temperature_examples/emontx_lowpower_temperature/emontx_lowpower_temperature.ino
+++ emonTx_temperature_examples/emontx_lowpower_temperature/emontx_lowpower_temperature.ino
@@ -53,11 +53,8 @@
 #define freq RF12_433MHZ                                                // Frequency of RF12B module can be RF12_433MHZ, RF12_868MHZ or RF12_915MHZ. You should use the one matching the module you have.
 const int nodeID = 18;                                                  // emonTx temperature RFM12B node ID - should be unique on network
 const int networkGroup = 210;                                           // emonTx RFM12B wireless network group - needs to be same as emonBase and emonGLCD
-<<<<<<< HEAD
                                                                                  //DS18B20 resolution 9,10,11 or 12bit corresponding to (0.5, 0.25, 0.125, 0.0625 degrees C LSB), lower resolution means lower power
-=======
-                                            
->>>>>>> 5db2b8a5b2c7f36ad0882060fc25965b2462f464
+
 const int time_between_readings= 20000;                                  //in ms
 
 #include <JeeLib.h>                                                     // Download JeeLib: http://github.com/jcw/jeelib
