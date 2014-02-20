a1d8a0e8fb694fc6ec2e422f060c9091c84cc50a
TrystanLea <glyn.hudson@openenergymonitor.org>
2014-02-20 08:59:41 +0000

    Direct serial example for v3

---
+++ emonTxV3/RFM12B/Examples/emonTxV3_DirectSerial/emonTxV3_DirectSerial.ino
@@ -0,0 +1,67 @@
+// EmonTx V3 Direct Serial example
+
+// Tell emonLib this is the emonTx V3
+// - don't read Vcc assume Vcc = 3.3V as is always the case on emonTx V3
+// - eliminates bandgap error and need for calibration http://harizanov.com/2013/09/thoughts-on-avr-adc-accuracy/
+#define emonTxV3
+
+// Include Emon Library
+#include "EmonLib.h" 
+
+// Create four instances
+EnergyMonitor ct1, ct2, ct3, ct4;
+
+unsigned long lastpost = 0;
+
+void setup()
+{    
+  Serial.begin(9600);
+  Serial.println("emonTx V3 Direct serial Example");
+  
+  // Calibration, phase_shift
+  ct1.voltage(0, 276.9, 1.7);                   
+  ct2.voltage(0, 276.9, 1.7);
+  ct3.voltage(0, 276.9, 1.7);
+  ct4.voltage(0, 276.9, 1.7);
+  
+  // CT Current calibration 
+  // (2000 turns / 22 Ohm burden resistor = 90.909)
+  ct1.current(1, 90.9); 
+  ct2.current(2, 90.9);
+  ct3.current(3, 90.9);
+  
+  // CT 4 is high accuracy @ low power -  4.5kW Max 
+  // (2000 turns / 120 Ohm burden resistor = 16.66)
+  ct4.current(4, 16.6);
+  
+  lastpost = 0;
+}
+
+void loop()
+{ 
+  // A simple timer to fix the post rate to once every 10 seconds
+  // Please dont post faster than once every 5 seconds to emoncms.org
+  // Host your own local installation of emoncms for higher resolutions
+  if ((millis()-lastpost)>=10000)
+  {
+    lastpost = millis();
+    
+    // .calcVI: Calculate all. No.of half wavelengths (crossings), time-out 
+    ct1.calcVI(20,2000);
+    ct2.calcVI(20,2000);
+    ct3.calcVI(20,2000);
+    ct4.calcVI(20,2000); 
+    
+    // Print to serial
+    Serial.print(ct1.realPower); Serial.print(' '); 
+    Serial.print(ct2.realPower); Serial.print(' '); 
+    Serial.print(ct3.realPower); Serial.print(' '); 
+    Serial.print(ct4.realPower); Serial.print(' '); 
+    Serial.println(ct1.Vrms);
+    
+    // Note: the following measurements are also available:
+    // - ct1.apparentPower
+    // - ct1.Vrms
+    // - ct1.Irms
+  }
+}
