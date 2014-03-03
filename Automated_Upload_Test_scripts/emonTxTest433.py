a00af06b04459510419e69dafc20e08bba7c8048
Glyn Hudson <glyn.hudson@gmail.com>
2014-03-03 13:28:18 +0000

    improve automated test and upload script

--- Automated_Upload_Test_scripts/emonTxTest433.py
+++ Automated_Upload_Test_scripts/emonTxTest433.py
@@ -1,54 +1,89 @@
+# Script for RaspberryPi with RFM12Pi 
+# Automated upload an test 
+# Upload code to emonTx and check for RF data received 
+
+# By Glyn Hudson 3/03/13
+# Part of the openenergymonitor.org project
+
 
 import serial, sys, string, commands, time, subprocess
 from subprocess import Popen, PIPE, STDOUT
 
-freq = '4b'
-group = '210g'
-
-print 'emonTx V3 433Mhz'
-
-print 'setting Rx module to receive on 433Mhz....'
-time.sleep(2) #delay 2 seconds
-ser = serial.Serial('/dev/ttyAMA0', 9600)
-ser.write(freq)
-time.sleep(0.5)
-linestr = ser.readline()
-print linestr
-if freq in linestr: 		#check RFM12Pi responce to check it's set frequenct OK	
-	print 'success..Rx Module set to receive on'
-	print freq  
-else:
-	print 'error..Rx Module is not responding'
-linestr=''
-time.sleep(1)
-ser.write(group)
-time.sleep(1)
-linestr = ser.readline()
-print linestr
-if group in linestr: 		#check RFM12Pi responce to check it's set frequenct OK	
-	print 'success..Rx Module set to receive on group'
-	print group  
-else:
-	print 'error..Rx Module is not responding'
-
-ser.close()
+print' '
+print 'OpenEnergyMonitor emonTx V3 Automated Upload & RF test'
+print ' ' 
+print 'Select desired frequency upload'
 
 
 
+while(1):
+	print ' ' 
+	nb = input('Enter (4) for 433Mhz or (8) for 868Mhz>')
+	#print ('Number%s \n' % (nb))
 
-	
 
-#print commands.getstatusoutput('avrdude -v  -c arduino -p ATMEGA328P -P /dev/ttyUSB0 -b 115200 -U flash:w:emonTxFirmware/emonTxV3/RFM12B/emonTxV3_RFM12B_DiscreteSampling/emonTxV3_RFM12B_DiscreteSampling.cpp433.hex')
+	if nb==4: 
+		print 'emonTx V3 433Mhz'
+		freq = '4b'
+		NodeID = 10
+		print 'setting Rx module to receive on 433Mhz....'
+		#time.sleep(1) #delay 2 seconds
+		ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
+		ser.write(freq)
+		#time.sleep(0.5)
+		linestr = ser.readline()
+		#print linestr
+		if freq in linestr: 		#check RFM12Pi responce to check it's set frequenct OK	
+			print 'success..Rx Module set to receive on 433Mhz'
+			#print freq  
+		else:
+			print 'error..Rx Module is not responding'
+		ser.close()
+		print 'Attempting 433Mhz emonTx V3 firmware upload....'
+		cmd = 'avrdude -u  -c arduino -p ATMEGA328P -P /dev/ttyUSB0 -b 115200 -U flash:w:emonTxFirmware/emonTxV3/RFM12B/emonTxV3_RFM12B_DiscreteSampling/emonTxV3_RFM12B_DiscreteSampling.cpp433.hex'
+		subprocess.call(cmd, shell=True)
 
-cmd = 'avrdude -u  -c arduino -p ATMEGA328P -P /dev/ttyUSB0 -b 115200 -U flash:w:emonTxFirmware/emonTxV3/RFM12B/emonTxV3_RFM12B_DiscreteSampling/emonTxV3_RFM12B_DiscreteSampling.cpp433.hex'
-subprocess.call(cmd, shell=True)
+		ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
+		linestr = ser.readline()
+		#print linestr
+		#print len(linestr)
+		if (len(linestr)>0):
+			if (int(linestr[1] + linestr[2])==NodeID): 
+				print 'PASS!...RF RECEIVE SUCCESS from emonTx'
+			else:
+				print 'FAIL...RF received but not from the emonTx'
+		ser.close()
 
-#p = Popen('svnadmin verify /var/svn/repos/config', stdout = PIPE, 
- #       stderr = STDOUT, shell = True)
-#time.sleep(2)	# 2 second delay
-#print "starting avrdude upload emonTx V3 433Mhz"
+	if nb==8: 
+		print 'emonTx V3 868Mhz'
+		freq = '8b'
+		NodeID = 10
+		print 'setting Rx module to receive on 833Mhz....'
+		#time.sleep(2) #delay 2 seconds
+		ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
+		ser.write(freq)
+		#time.sleep(0.5)
+		linestr = ser.readline()
+		#print linestr
+		if freq in linestr: 		#check RFM12Pi responce to check it's set frequenct OK	
+			print 'success..Rx Module set to receive on 868Mhz'
+			#print freq  
+		else:
+			print 'error..Rx Module is not responding'
+		ser.close()
+		print 'Attempting 868Mhz emonTx V3 firmware upload....'
+		cmd = 'avrdude -u  -c arduino -p ATMEGA328P -P /dev/ttyUSB0 -b 115200 -U flash:w:emonTxFirmware/emonTxV3/RFM12B/emonTxV3_RFM12B_DiscreteSampling/emonTxV3_RFM12B_DiscreteSampling.cpp868.hex'
+		subprocess.call(cmd, shell=True)
 
-ser = serial.Serial('/dev/ttyAMA0', 9600)
-linestr = ser.readline()
-print linestr
-ser.close()
+		ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
+		linestr = ser.readline()
+		#print linestr
+		if (len(linestr)>0):
+			if (int(linestr[1] + linestr[2])==NodeID): 
+				print 'PASS!...RF RECEIVE SUCCESS from emonTx'
+			else:
+				print 'FAIL...RF received but not from the emonTx'
+		ser.close()
+
+	if ((nb!=8) and (nb!=4)):
+		print 'Invalid selection, please restart script and select 4 or 8'
